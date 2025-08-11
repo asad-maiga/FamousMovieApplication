275b49a19149d0d103ef4907663dbc835565fec3
asadmaiga <asadmaiga45@gmail.com>
2025-08-11 16:31:35 +0300

    This is a small react project for searching famous movies

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import "./css/App.css"
+import Home from "./pages/Home"
+import Favorites from './pages/Favorites'
+import {Routes, Route} from "react-router-dom"
+import NavBar from "./components/NavBar"
+import { MovieProvider } from "./context/MovieContext"
+
+
+function App() {
+  return (
+    <MovieProvider>
+      <NavBar />
+      <main className="maint-content">
+        <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='/favorites' element={<Favorites />}/>
+
+        </Routes>
+      </main>
+    </MovieProvider>
+  );
+}
+
+export default App
